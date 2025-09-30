f00ba6b1893efc7a69600bf35a2827debff0d01a
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-09-30 21:45:59 +0000

    Add MongoDB connection with `.env` support and integrate `GetMovies` functionality in `movie_controller`

--- server/controller/movie_controller.go
+++ server/controller/movie_controller.go
@@ -1,9 +1,43 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/elisha1995/magic-stream-movies/server/database"
+	"github.com/elisha1995/magic-stream-movies/server/model"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+var movieCollection = database.OpenCollection("movies")
 
 func GetMovies() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "List of movies"})
+		ctx, cancel := context.WithTimeout(c, 100*time.Second)
+		defer cancel()
+
+		cursor, err := movieCollection.Find(ctx, bson.D{})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch movies."})
+		}
+		defer func(cursor *mongo.Cursor, ctx context.Context) {
+			err := cursor.Close(ctx)
+			if err != nil {
+				panic(err)
+			}
+		}(cursor, ctx)
+
+		var movies []model.Movie
+
+		if err = cursor.All(ctx, &movies); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode movies."})
+			return
+		}
+
+		c.JSON(http.StatusOK, movies)
 	}
 }

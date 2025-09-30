e7052d4fc08102f1384fc27f516481fd1d57f84d
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-09-30 20:48:52 +0000

    Add `/movies` route with `GetMovies` handler in `movie_controller`

--- server/main.go
+++ server/main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	controller "github.com/elisha1995/magic-stream-movies/server/controller"
 )
 
 func main() {
@@ -13,6 +15,8 @@ func main() {
 	router.GET("/hello", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello, MagicStreamMovies")
 	})
+
+	router.GET("/movies", controller.GetMovies())
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server: ", err)

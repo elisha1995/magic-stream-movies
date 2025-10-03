37eb2a7eeecccf2be7cdf6c799967903d9594fac
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-10-03 14:19:17 +0000

    Integrate MongoDB connection in `main.go` and refactor route setup functions to accept `*mongo.Client`.

--- server/main.go
+++ server/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/elisha1995/magic-stream-movies/server/database"
 	"github.com/elisha1995/magic-stream-movies/server/route"
 	"github.com/gin-gonic/gin"
 )
@@ -15,8 +18,22 @@ func main() {
 		c.String(http.StatusOK, "Hello, MagicStreamMovies")
 	})
 
-	route.SetupUnProtectedRoutes(router)
-	route.SetupProtectedRoutes(router)
+	var client = database.Connect()
+
+	if err := client.Ping(context.Background(), nil); err != nil {
+		log.Fatalf("Failed to reach mongodb server: %v", err)
+	}
+
+	defer func() {
+		err := client.Disconnect(context.Background())
+		if err != nil {
+			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
+		}
+
+	}()
+
+	route.SetupUnProtectedRoutes(router, client)
+	route.SetupProtectedRoutes(router, client)
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server: ", err)
